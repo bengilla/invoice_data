78d87d6deff98a7050e526d3189a552cce5abb10
bengilla <bengilla@outlook.com>
2023-02-26 09:00:47 +0800

    update redirect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
-from fastapi import FastAPI, Request, File, UploadFile
+from fastapi import FastAPI, Request, File, UploadFile, status
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from models.bill_scan import QRCode
 from models.mongodb import MongoDB
@@ -11,24 +12,32 @@ _db = MongoDB()
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
 
-@app.get("/{num}")
-async def index(request: Request):
+def list_collection():
     list_collection = [x for x in _db.list_collections()]
-    invoice_data = [x for x in _db.send_data(1).find({})]
-    
+    return sorted(list_collection)
+
+num = list_collection()[0]
+
+@app.get("/", response_class=RedirectResponse, status_code=302)
+async def index():
+    return f"/{num}"
+
+@app.get("/{num}", response_class=HTMLResponse)
+async def first(request: Request, num: str):
+    invoice_data = [x for x in _db.send_data(num).find({})]
     amount = [x['amount'] for x in invoice_data] 
 
     return templates.TemplateResponse(
         "index.html",
         {
         "request": request,
-        "list_col": sorted(list_collection),
+        "list_col": list_collection(),
         "data": invoice_data,
         "total": sum(amount)
         }
     )
 
-@app.post("/")
+@app.post("/", response_class=HTMLResponse)
 async def send_file(request: Request, image: UploadFile = File(None)):
     qrcode = QRCode()
     with Image.open(image.file) as im:

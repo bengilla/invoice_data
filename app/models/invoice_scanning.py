083fd914c8f3afda32d9edbe57392599e541adb3
bengilla <bengilla@outlook.com>
2023-03-06 10:23:20 +0800

    Update invoice_scanning.py

--- app/models/invoice_scanning.py
+++ app/models/invoice_scanning.py
@@ -16,7 +16,7 @@ class Invoice:
     def __init__(self) -> None:
         self.date = None
 
-    def get_data(self, content: list[str]):
+    def get_data(self, content):
         """take piece by piece convert to final data (dict)"""
         store_data = {"date": [], "code": [], "number": [], "amount": []}
         title_data = {"date": "开票日期", "code": "发票代码", "number": "发票号码"}

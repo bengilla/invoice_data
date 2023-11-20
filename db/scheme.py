7df4ce8310e270ae94f3cf4c232a8a3a33e14a96
bengilla <bengilla@outlook.com>
2023-11-20 08:58:09 +0800

    Update scheme.py

--- db/scheme.py
+++ db/scheme.py
@@ -27,7 +27,7 @@ class UserScheme(Base):
     username = Column("username", String(255), nullable=False, unique=True)
     password = Column("password", String(255), nullable=False)
     register_date = Column("register_date", DateTime, default=datetime.now())
-    invoice = relationship("InvoiceSchema", backref="user")
+    invoice = relationship("InvoiceScheme", backref="users")
 
 
 class InvoiceScheme(Base):

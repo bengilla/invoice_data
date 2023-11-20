6538a47ec755cd96da538abaa2e9a373ab907498
bengilla <bengilla@outlook.com>
2023-11-20 08:47:11 +0800

    Update scheme.py

--- db/scheme.py
+++ db/scheme.py
@@ -1,16 +1,16 @@
-import uuid
 from datetime import datetime
 from sqlalchemy import (
     create_engine,
     Column,
     String,
     DateTime,
-    Uuid,
     Integer,
     Float,
     Boolean,
-    BINARY,
+    ForeignKey,
 )
+from sqlalchemy.dialects.mysql import LONGTEXT
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 from config.settings import Settings
@@ -23,29 +23,22 @@ engine = create_engine(_settings.MySQL, echo=True)
 
 class UserScheme(Base):
     __tablename__ = "users"
-    id = Column("id", Uuid, primary_key=True, default=uuid.uuid1())
+    id = Column("id", Integer, primary_key=True)
     username = Column("username", String(255), nullable=False, unique=True)
     password = Column("password", String(255), nullable=False)
     register_date = Column("register_date", DateTime, default=datetime.now())
-
-    def __repr__(self):
-        return f"{self.id}, {self.username}, {self.password}, {self.register_date}"
+    invoice = relationship("InvoiceSchema", backref="user")
 
 
-# def invoice_models(tablename):
-#     class InvoiceScheme(Base):
-#         __tablename__ = tablename
-#         id = Column(Integer, primary_key=True, unique=True)
-#         date = Column(DateTime())
-#         company = Column(String())
-#         amount = Column(Float())
-#         pdf = Column(BINARY())
-#         download = Column(Boolean, default=False)
-
-#         def __repr__(self):
-#             return f"{self.id}, {self.date}, {self.company}, {self.amount}, {self.pdf}, {self.download}"
-
-#     return InvoiceScheme()
+class InvoiceScheme(Base):
+    __tablename__ = "invoices"
+    id = Column("id", Integer(), primary_key=True, unique=True)
+    date = Column("date", DateTime())
+    company = Column("company", String(255))
+    amount = Column("amount", Float())
+    pdf = Column("pdf", LONGTEXT)
+    download = Column("download", Boolean, default=False)
+    user_id = Column(Integer, ForeignKey("users.id"))
 
 
 Base.metadata.create_all(bind=engine)
